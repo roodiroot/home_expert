34d3f80781910dc4f52b0b375b5d7834d416c0c5
Borisov Maxim <voodivood@gmail.com>
2024-01-23 11:45:28 +0300

    add push for admin

--- src/app/api/order/telegram.ts
+++ src/app/api/order/telegram.ts
@@ -1,15 +1,19 @@
 import { makePostRequest } from "./api";
 
 const telegramBotKey = process.env.BOT_TOKEN;
-const chat_id = process.env.CHAT_ID;
+const chat = [process.env.CHAT_ID, process.env.CHAT_ID_ADMIN];
 
 export const sendNotification = async (text: string, parse_mode = "HTML") => {
   const endpoint = `https://api.telegram.org/bot${telegramBotKey}/sendMessage`;
-  const response = await makePostRequest(endpoint, {
-    text,
-    parse_mode,
-    chat_id,
-  });
+  let response;
+  for (let i = 0; i < chat.length; i++) {
+    response = await makePostRequest(endpoint, {
+      text,
+      parse_mode,
+      chat_id: chat[i],
+    });
+  }
+
   if (!response) {
     return null;
   }
